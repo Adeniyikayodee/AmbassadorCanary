ae2ed5c739a38bb06e37754abdabea5122887d1b
Adeniyikayodee <adeniyikayode22@gmail.com>
2023-02-12 23:48:12 +0500

    First commit

---
+++ backend/content/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const content = require("./route");
+
+// Data to be fetched by the API
+
+app.use("/v1/content",content);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
